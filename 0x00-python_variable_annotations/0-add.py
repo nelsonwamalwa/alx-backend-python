fdf7287fe2a2dfccf1aa967694a73e03e292cd65
NelsonWamalwa <wamalwanelson@gmail.com>
2024-04-18 22:21:26 +0300

    Task 0

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""This is a module that contains a single function that adds two integers."""
+
+
+def add(a: float, b: float) -> float:
+    """ Adding two integers together."""
+    return a + b
