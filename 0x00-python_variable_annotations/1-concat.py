ff429a20c38fab41cab39c2f1ecf97f44a465d05
NelsonWamalwa <wamalwanelson@gmail.com>
2024-04-18 22:24:05 +0300

    Task 0

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""This a single function that concatenates two strings."""
+
+
+def concat(str1: str, str2: str) -> str:
+    """Concatenates two strings."""
+    return str1 + str2
