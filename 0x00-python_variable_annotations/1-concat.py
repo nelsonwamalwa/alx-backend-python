1eebecf67e27928633ad4977a91f5ce31776adec
NelsonWamalwa <wamalwanelson@gmail.com>
2024-04-18 22:26:52 +0300

    Task 1

--- 0x00-python_variable_annotations/1-concat.py
+++ 0x00-python_variable_annotations/1-concat.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
-"""This a single function that concatenates two strings."""
+""" Module documentation """
 
 
 def concat(str1: str, str2: str) -> str:
-    """Concatenates two strings."""
+    """Func doc"""
     return str1 + str2
