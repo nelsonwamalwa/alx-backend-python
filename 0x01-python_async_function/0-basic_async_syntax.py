f6f34d993d41b887f3b716cb0a2c2229fbf68751
NelsonWamalwa <wamalwanelson@gmail.com>
2024-04-22 22:50:19 +0300

    Task three

--- 0x01-python_async_function/0-basic_async_syntax.py
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-"Contains a coroutine that pauses for a specified duration before returning."
+"Includes a coroutine that halts execution for a specified period before yielding control back."
 
 import asyncio
 import random
@@ -7,15 +7,15 @@ import random
 
 async def wait_random(max_delay: int = 10) -> float:
     """
-This function generates a random decimal number
-within the range of 0 to the specified maximum delay.
+This function creates a random decimal number between 0 and a specified maximum delay.
 
 Parameters:
 
-max_delay: The upper limit for the delay time.
+- max_delay: The upper limit for the delay time.
 Returns:
-A randomly generated decimal number within 
-the range of 0 to max_delay.
+
+- A randomly generated decimal number within the range of 0 to max_delay.
+
     """
     rand = random.uniform(0, max_delay)
     await asyncio.sleep(rand)
--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -1,9 +1,5 @@
 #!/usr/bin/env python3
-"""
-This includes a function that initiates the execution of
-wait_random n times, with a set interval between each invocation.
-
-"""
+"This contains a function that starts the execution of wait_random n times, with a defined interval between each call."
 import asyncio
 from typing import List
 
@@ -11,11 +7,10 @@ wait_random = __import__('0-basic_async_syntax').wait_random
 
 
 async def wait_n(n: int, max_delay: int) -> List[float]:
-    """Generate 'wait_random' function calls 'n' times with 
-    a designated delay between each invocation. Parameters 
-    include 'n' for the number of iterations and 'max_delay' 
-    for the maximum duration between calls. The function 
-    returns a list containing the delays.
-    """
+   """Generate calls to the 'wait_random' function 'n' times, 
+   with a specified delay between each call. Parameters include 
+   'n' for the number of iterations and 'max_delay' for the 
+   maximum duration between calls. The function returns a list 
+   containing the delays."""
     tasks = [asyncio.create_task(wait_random(max_delay)) for _ in range(n)]
     return [await task for task in asyncio.as_completed(tasks)]
