fa1ce2dd3e8486010209c7555ffdeacc9f0a1a6c
NelsonWamalwa <wamalwanelson@gmail.com>
2024-04-23 17:43:01 +0300

    Assignment two

--- 0x02-python_async_comprehension/2-measure_runtime.py
+++ 0x02-python_async_comprehension/2-measure_runtime.py
@@ -8,11 +8,11 @@ async_comprehension = __import__('1-async_comprehension').async_comprehension
 
 async def measure_runtime() -> float:
     """
-    measure_runtime - a function execute async_com 4 times
+    measure_runtime -A function execute async_com 4 times
     Arguments:
         nothing
     Returns:
-        total exection time required to complete the task
+        The total exection time required to complete the task
     """
     t_start = time.perf_counter()
     task = [async_comprehension() for i in range(4)]
