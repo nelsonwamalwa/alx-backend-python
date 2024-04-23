24989957e3f961629a61f6051979b4f5b5bc0cbe
NelsonWamalwa <wamalwanelson@gmail.com>
2024-04-23 17:51:26 +0300

    Assignment two

--- 0x02-python_async_comprehension/2-measure_runtime.py
+++ 0x02-python_async_comprehension/2-measure_runtime.py
@@ -1,22 +1,16 @@
 #!/usr/bin/env python3
-"""
-This module measures the execution time of an asynchronous comprehension.
-"""
-
-import time
+""" Module documentation """
 import asyncio
-async_comprehension = __import__('1-async_comprehension').async_comprehension
+import time
+
+async_comprehension = __import__("1-async_comprehension").async_comprehension
+
 
 async def measure_runtime() -> float:
-    """
-    measure_runtime - Executes async_comprehension 4 times and measures the total execution time.
-    
-    Returns:
-        The total execution time required to complete the task.
-    """
-    t_start = time.perf_counter()
-    tasks = [async_comprehension() for _ in range(4)]
-    await asyncio.gather(*tasks)
-    t_end = time.perf_counter()
-    return t_end - t_start
-
+    """Function documentation"""
+    start = time.perf_counter()
+    await asyncio.gather(async_comprehension(), async_comprehension(),
+                         async_comprehension(), async_comprehension()
+                         )
+    end = time.perf_counter()
+    return end - start
